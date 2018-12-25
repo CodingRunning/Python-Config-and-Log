5b98af70584ace18c4c16b94f5f1f85a689b1ae0
CodingRunning <44487909+CodingRunning@users.noreply.github.com>
2018-12-25 17:19:15 +0800

    Add files via upload

--- logtest.py
+++ logtest.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
-
+'''
 from urllib import urlopen
 
 url=r'https://www.baidu.com/'
-
 log=open('logfile.txt','w')
 print>>log,('Downloading file from %s') % url
 text=urlopen(url).read()
 print>>log,('File successfully download')
+'''
 
+import logging
+logging.basicConfig(level=logging.INFO,filename='logfile.txt')
+logging.info('Start program')
+logging.info('Trying to divide 2 by 2')
+print 2/2
+logging.info('The division succeeded')
+logging.info('Trying to divide 2 by 0')
+print 2/0
+logging.info('The division succeeded')
+logging.info('Ending program')
 
